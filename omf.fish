a0c1a870c2d47ba420bb7f4bb32ed19bc07fc3b7
Lukáš Huvar <lukas@huvar.cz>
2019-03-04 11:49:02 +0100

    Add docker commands

--- omf.fish
+++ omf.fish
@@ -37,3 +37,17 @@ alias pr="prisma"
 # Shortcuts
 alias s="yarn start"
 alias e="yarn electron-start"
+
+# Docker
+function remove-containers
+  docker stop (docker ps -aq)
+  docker rm (docker ps -aq)
+end
+
+function cleanup-docker
+  remove-containers
+  docker network prune -f
+  docker rmi -f (docker images --filter dangling=true -qa)
+  docker volume rm (docker volume ls --filter dangling=true -q)
+  docker rmi -f (docker images -qa)
+end
